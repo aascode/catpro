1df3d4f8c8f978ccc9704c9e8263fa647d5d9609
Daniel Mark Low <daniel.m.low@gmail.com>
2019-03-04 20:12:54 -0500

    sbatch

--- catpro/run_gpt2.sh
+++ catpro/run_gpt2.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 #
-#SBATCH --time=7:00:00
-#SBATCH --mem=32GB
+#SBATCH --time=0:05:00
+#SBATCH --mem=2GB
 #SBATCH --gres=gpu:GEFORCEGTX1080TI:1
 #SBATCH -c4
 
+cd /om2/user/dlow/containers/
 module add openmind/singularity/3.0
 singularity shell -B /om2/ catpro.simg
 cd /home/dlow/catpro/catpro/
